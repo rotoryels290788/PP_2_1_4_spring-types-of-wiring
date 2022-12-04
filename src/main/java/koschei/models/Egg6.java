4a27a29e3d6d202139ec69b7145eee94b611c1d3
Dmitry Kharin <104873585+rotoryels290788@users.noreply.github.com>
2022-12-04 16:45:00 +0300

    Update Egg6.java

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,20 +1,15 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class Egg6 {
-
-    private Needle7 needle7;
-    @Autowired
-    public Egg6(Needle7 needle7){
-        this.needle7=needle7;
+    private final Needle7 needle;
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
     }
-
     @Override
     public String toString() {
-        return ", в яйце иголка " + needle7.toString();
+        return ", в яйце иголка " + needle.toString() ;
     }
 }
